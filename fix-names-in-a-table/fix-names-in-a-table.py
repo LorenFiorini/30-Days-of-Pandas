58c60ad1b6069f81f270b4e02de0f4ae8218d1e6
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2025-04-15 19:42:15 +0200

    fix-names-in-a-table

--- fix-names-in-a-table/fix-names-in-a-table.py
+++ fix-names-in-a-table/fix-names-in-a-table.py
@@ -1,6 +1,9 @@
+
 import pandas as pd
 
 def fix_names(users: pd.DataFrame) -> pd.DataFrame:
     users['name'] = users['name'].str.capitalize()
-    # print(users['name'])
+    print(users)
+    users.sort_values(['user_id'], inplace=True)
     return users
+
