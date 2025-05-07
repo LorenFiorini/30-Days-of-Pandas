34cde25d2672e7906a1c748bf1eacc29529a0edd
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2025-05-07 18:59:51 +0200

    second_highest_salary

---
+++ second-highest-salary/second-highest-salary.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    unique_salaries = employee['salary'].unique()
+    print(unique_salaries)
+    if len(unique_salaries) < 2:
+        return pd.DataFrame(
+            {
+                'SecondHighestSalary':[None]
+            }
+        )
+
+    return pd.DataFrame(
+        {
+            'SecondHighestSalary': [unique_salaries[1]]
+        }
+    )
