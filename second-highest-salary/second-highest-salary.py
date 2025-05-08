dd0d0b11951f042775202dfa626b078ce858fcc8
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2025-05-08 10:24:45 +0200

    find_products()

--- second-highest-salary/second-highest-salary.py
+++ second-highest-salary/second-highest-salary.py
@@ -3,6 +3,11 @@ import pandas as pd
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     unique_salaries = employee['salary'].unique()
     print(unique_salaries)
+    # print(type(unique_salaries))
+    np.sort(unique_salaries)
+    unique_salaries[::-1].sort()
+    # unique_salaries.flip()
+    print(unique_salaries)
     if len(unique_salaries) < 2:
         return pd.DataFrame(
             {
